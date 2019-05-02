f511f892065c706a204d08a70c78c7304f77aef9
Andreu <a.raig@oakhouseschool.com>
2019-05-02 22:53:16 +0200

    Update servo_condition.ino
    Corregit algun error i afegit uns delay

--- servo_condition.ino
+++ servo_condition.ino
@@ -13,15 +13,17 @@ void loop()
   condition = get_condition();
   if (condition)
   {
-    motor.write(180);
+    motor.write(180
+    delay(1000);
   }
   else
   {
     motor.write(0);
+    delay(1000);
   }
 }
 
 bool get_condition()
 {
-  return !condition
+  return !condition;
 }
